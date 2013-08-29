81fd244633d7dba8316a1511ca241993dc4bdf72
Noah Moroze <noahm@moroze.com>
2013-08-28 23:24:25 -0400

    Added stuff to admin site

---
+++ crush/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from edit.models import *
+
+admin.site.register(Classes)
+admin.site.register(Preference)
+admin.site.register(User_profile)
+admin.site.register(SchoolProfile)
