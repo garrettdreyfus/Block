e9f9d9a0b11cc2815ad71600dcf4821b94846d1d
Garrett Finucane <garrettdreyfus@gmail.com>
2013-08-28 23:56:25 -0400

    AYAYAYYAYAY

--- crushrank/urls.py
+++ crushrank/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-	url(r'^/', include('crush.urls')),
+	url(r'^', include('crush.urls')),
     url(r'^admin/', include(admin.site.urls)),
     
 )
